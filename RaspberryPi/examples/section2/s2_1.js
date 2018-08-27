53225b4abcdb1de70b6c16d9dc42a5156a8a35ff
Satoru Takagi <satakagi@users.noreply.github.com>
2018-08-27 09:41:24 +0900

    Add files via upload

---
+++ RaspberryPi/examples/section2/s2_1.js
@@ -0,0 +1,26 @@
+'use strict'; // strictモードで実行。細かいエラーチェックが行われます。
+
+var head;
+window.addEventListener('load', function (){
+	head = document.querySelector('#ADT7410value');
+	mainFunction();
+}, false);
+
+
+async function mainFunction(){
+	var i2cAccess = await navigator.requestI2CAccess(); // i2cAccessを非同期で取得
+	var port = i2cAccess.ports.get(1); // I2C I/Fの1番ポートを取得
+	var adt7410 = new ADT7410(port,0x48); // 取得したポートの0x48アドレスをADT7410ドライバで受信する
+	await adt7410.init();
+	while(1){ // 無限ループ
+		var value = await adt7410.read();
+		head.innerHTML = value ? value+"degree" : "Measurement failure";
+		await sleep(1000);
+	}
+}
+
+function sleep(ms){
+	return new Promise( function(resolve) {
+		setTimeout(resolve, ms);
+	});
+}
---
+++ RaspberryPi/examples/section2/s2_2.js
@@ -0,0 +1,30 @@
+// ADT7410valueドライバを使わず、自力でADT7410の値を読むサンプル
+
+'use strict'; // strictモードで実行。細かいエラーチェックが行われます。
+
+var head;
+window.addEventListener('load', function (){
+	head = document.querySelector('#ADT7410value');
+	mainFunction();
+}, false);
+
+
+async function mainFunction(){
+	var i2cAccess = await navigator.requestI2CAccess(); // i2cAccessを非同期で取得
+	var port = i2cAccess.ports.get(1); // I2C I/Fの1番ポートを取得
+	var i2cSlaveDevice = await port.open(0x48); // アドレス0x48のI2Cスレーブデバイスを得る
+	
+	while(1){ // 無限ループ
+		var MSB = await i2cSlaveDevice.read8(0x00); // これ以下の３行が肝です
+		var LSB = await i2cSlaveDevice.read8(0x01);
+		var temperature = ((MSB << 8)|(LSB & 0xff))/128.0;
+		head.innerHTML = temperature + "℃";
+		await sleep(1000);
+	}
+}
+
+function sleep(ms){
+	return new Promise( function(resolve) {
+		setTimeout(resolve, ms);
+	});
+}
