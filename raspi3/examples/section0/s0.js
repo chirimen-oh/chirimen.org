cbac9ea344b1e507707dcb5e8fa8872c75a9b126
dynamis <dynamis@firehacks.org>
2018-10-31 22:35:51 +0900

    add globals section (for sleep())

--- raspi3/examples/section0/s0.js
+++ raspi3/examples/section0/s0.js
@@ -1,24 +1,24 @@
 async function mainFunction() {
-  // プログラムの本体となる関数、非同期処理のためプログラム全体をasync関数で包みます。
-  var gpioAccess = await navigator.requestGPIOAccess(); // thenの前の関数をawait接頭辞をつけて呼び出します。
+  // プログラムの本体となる関数。非同期処理を await で扱えるよう全体を async 関数で包みます
+  var gpioAccess = await navigator.requestGPIOAccess(); // 非同期関数は await を付けて呼び出す
   var port = gpioAccess.ports.get(26);
   var v = 0;
 
   await port.export("out");
-  while (true) {
-    // 無限ループで 1000ms 毎に繰り返し処理を行う
-    await sleep(1000);
-    v = v === 0 ? 1 : 0; // vが1の場合はvが0に、0の場合は1に変化する。1でLED点灯、0で消灯するので、1秒間隔でLEDがON OFFする。
+  for (;;) {
+    // 無限ループ
+    await sleep(1000); // 無限ループの繰り返し毎に 1000ms 待機する
+    v = v === 0 ? 1 : 0; // vの値を0,1入れ替える。1で点灯、0で消灯するので、1秒間隔でLEDがON OFFする
     port.write(v);
   }
 }
 
-// この関数の定義方法はとりあえず気にしなくて良いです。コピペしてください。
-// 単に指定したms秒スリープするだけの非同期処理関数
+// await sleep(ms) と呼べば指定 ms 待機する非同期関数
+// 同じものが polyfill.js でも定義されているため省略可能
 function sleep(ms) {
   return new Promise(resolve => {
     setTimeout(resolve, ms);
   });
 }
 
-mainFunction(); // 定義したasync関数を実行します（このプログラムのエントリーポイントになっています）
+mainFunction(); // 定義したasync関数を実行します（このプログラムのエントリーポイント）
