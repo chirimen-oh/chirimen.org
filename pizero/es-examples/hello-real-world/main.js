207f6cc9817140d271dad9ff8a67a8f8243775dd
Satoru Takagi <satakagi@users.noreply.github.com>
2021-08-30 18:56:27 +0900

    Update main.js

--- pizero/es-examples/hello-real-world/main.js
+++ pizero/es-examples/hello-real-world/main.js
@@ -2,7 +2,6 @@ import {requestGPIOAccess} from "./node_modules/node-web-gpio/dist/index.js";
 const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
 
 async function blink() {
-    console.log(requestGPIOAccess);
   const gpioAccess = await requestGPIOAccess();
   const port = gpioAccess.ports.get(26);
 
