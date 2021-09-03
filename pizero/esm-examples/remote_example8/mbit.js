ff3072863297a7f9250dfa405c8ebb88640445c8
Satoru Takagi <satakagi@gmail.com>
2021-09-03 17:53:43 +0900

    Add remote examples on pizero

---
+++ pizero/esm-examples/remote_example8/mbit.js
@@ -0,0 +1,43 @@
+// Remote Example8 - reciever
+// for CHIRIMEN with nodejs
+
+import {requestI2CAccess} from "./node_modules/node-web-i2c/index.js";
+import PCA9685 from "@chirimen/pca9685";
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
+import nodeWebSocketLib from "websocket"; // https://www.npmjs.com/package/websocket
+import {RelayServer} from "./RelayServer.js";
+
+var channel;
+var pca9685;
+
+async function connect(){
+	// chirimen with micro:bit、サーボコントローラの初期化
+	var i2cAccess = await requestI2CAccess();
+	var i2cPort = i2cAccess.ports.get(1);
+	pca9685 = new PCA9685(i2cPort, 0x40);
+	await pca9685.init(0.001, 0.002, 30);
+	
+	
+	// webSocketリレーの初期化
+	var relay = RelayServer("achex", "chirimenSocket" , nodeWebSocketLib, "https://chirimen.org");
+	channel = await relay.subscribe("chirimenMbitRemoteServo");
+	console.log("achex web socketリレーサービスに接続しました");
+	channel.onmessage=moveServo;
+}
+
+async function moveServo(message) {
+//	console.log("message:",message);
+	if ( !message.data.slope){
+		return;
+	}
+	var angle = message.data.slope;
+	if ( Math.abs(angle)>28){
+		angle = Math.sign(angle)*28;
+	}
+	console.log("servo:",angle);
+    await pca9685.setServo(0, angle);
+    console.log("サーボ角度:"+angle);
+	channel.send({setAngle:angle});
+}
+
+connect();
---
+++ pizero/esm-examples/remote_example9/pc.js
@@ -0,0 +1,16 @@
+// Remote Example4 - controller
+import {RelayServer} from "./RelayServer.js";
+
+var channel;
+onload = async function(){
+	// webSocketリレーの初期化
+	var relay = RelayServer("achex", "chirimenSocket" );
+	channel = await relay.subscribe("chirimenSW");
+	messageDiv.innerText="achex web socketリレーサービスに接続しました";
+	channel.onmessage = getMessage;
+}
+
+function getMessage(msg){ // メッセージを受信したときに起動する関数
+	messageDiv.innerText = "スイッチが、"+msg.data+"になりました。";
+}
+
