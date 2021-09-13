525729ad777e4973f1f346a7517dd5f65298cd62
Satoru Takagi <satakagi@users.noreply.github.com>
2021-09-13 12:17:59 +0900

    Create pc.js

---
+++ pizero/esm-examples/remote_vl53l0x/pc.js
@@ -0,0 +1,20 @@
+// Remote Example1 - controller
+import {RelayServer} from "./RelayServer.js";
+
+window.getData = getData;
+
+var channel;
+onload = async function(){
+	// webSocketリレーの初期化
+	var relay = RelayServer("achex", "chirimenSocket" );
+	channel = await relay.subscribe("chirimenVL");
+	messageDiv.innerText="achex web socketリレーサービスに接続しました";
+	channel.onmessage = getMessage;
+}
+
+function getMessage(msg){ // メッセージを受信したときに起動する関数
+	var mdata = msg.data;
+	messageDiv.innerText = JSON.stringify(mdata);
+	console.log("mdata:",mdata);
+	distanceTd.innerText = mdata;
+}
