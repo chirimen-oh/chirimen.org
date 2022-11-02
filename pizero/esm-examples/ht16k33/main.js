682248183c3056e13e7a067ba2b9111f22260fb0
Satoru Takagi <satakagi@users.noreply.github.com>
2022-11-02 22:37:02 +0900

    Update main.js

--- pizero/esm-examples/ht16k33/main.js
+++ pizero/esm-examples/ht16k33/main.js
@@ -21,6 +21,28 @@ const iconPattern =[
 0,0,1,1,1,1,0,0,
 ]; // スマイルマーク
 
+const iconPattern2 =[
+1,0,0,0,0,0,0,1,
+0,1,0,0,0,0,1,0,
+0,0,1,1,1,1,0,0,
+0,1,1,1,1,1,1,0,
+1,1,0,1,1,0,1,1,
+0,1,1,1,1,1,1,0,
+0,0,1,0,0,1,0,0,
+1,1,0,0,0,0,1,1
+]; // インベーダー
+
+const iconPattern3 =[
+0,1,0,0,0,0,1,0,
+1,1,1,1,1,1,1,0,
+1,0,0,1,0,0,1,0,
+1,1,0,1,1,0,1,0,
+1,0,0,1,0,0,1,0,
+1,1,1,1,1,1,1,0,
+0,1,1,1,1,1,1,1,
+0,1,0,1,0,1,0,1
+]; // 犬
+
 async function main() {
 	const i2cAccess = await requestI2CAccess();
 	const port = i2cAccess.ports.get(1);
@@ -30,11 +52,25 @@ async function main() {
 	//await ht.set_blink(ht.HT16K33_BLINK_1HZ);
 	//await ht.set_brightness(6);
 	
-	ht.set_8x8_array(iconPattern);
-	/**
-	for ( var i = 0 ; i < 128 ; i++ ){
-		ht.set_led(i, 1);
+	while(true){
+		ht.set_8x8_array(iconPattern);
+		await ht.write_display();
+		await sleep(1000);
+		
+		ht.set_8x8_array(iconPattern2);
+		await ht.write_display();
+		await sleep(1000);
+		
+		ht.set_8x8_array(iconPattern3);
+		await ht.write_display();
+		await sleep(1000);
+		
+		/** LEDを一個づつ設定する関数の使用例
+		for ( var i = 0 ; i < 128 ; i++ ){
+			ht.set_led(i, 1);
+		}
+		await ht.write_display();
+		**/
+		await sleep(1000);
 	}
-	**/
-	await ht.write_display();
 }
