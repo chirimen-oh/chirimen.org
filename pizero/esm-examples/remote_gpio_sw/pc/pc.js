38c27dcce9a1b8a4429804a36508d445031e76b0
aya <77430617+AyAkADream@users.noreply.github.com>
2023-02-04 00:17:50 +0900

    initial commit

--- pizero/esm-examples/remote_gpio_sw/pc/pc.js
+++ pizero/esm-examples/remote_gpio_sw/pc/pc.js
@@ -1,16 +1,16 @@
 // Remote Example4 - controller
-import {RelayServer} from "https://chirimen.org/remote-connection/js/beta/RelayServer.js";
+import { RelayServer } from "https://chirimen.org/remote-connection/js/beta/RelayServer.js";
 
 var channel;
-onload = async function(){
-	// webSocketリレーの初期化
-	var relay = RelayServer("chirimentest", "chirimenSocket" );
-	channel = await relay.subscribe("chirimenSW");
-	messageDiv.innerText="web socketリレーサービスに接続しました";
-	channel.onmessage = getMessage;
-}
+onload = async function () {
+  // webSocketリレーの初期化
+  var relay = RelayServer("chirimentest", "chirimenSocket");
+  channel = await relay.subscribe("chirimenSW");
+  messageDiv.innerText = "web socketリレーサービスに接続しました";
+  channel.onmessage = getMessage;
+};
 
-function getMessage(msg){ // メッセージを受信したときに起動する関数
-	messageDiv.innerText = "スイッチが、"+msg.data+"になりました。";
+function getMessage(msg) {
+  // メッセージを受信したときに起動する関数
+  messageDiv.innerText = "スイッチが、" + msg.data + "になりました。";
 }
-
