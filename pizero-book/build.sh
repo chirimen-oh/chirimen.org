f3f18389443d2b0162b925f15eccb89f9ff1fa7f
akihiko.kigure <akihiko.kigure@gmail.com>
2023-10-01 16:15:58 +0900

    ディレクトリ構成変更に伴う build シェル 修正

--- pizero-book/build.sh
+++ pizero-book/build.sh
@@ -3,9 +3,13 @@
 clear
 
 echo 'remove old html'
-rm ../pizero/readme.html
-rm ../chirimenGeneric/readme.html
-rm ./pizero-book.pdf
+rm toc.html
+find docs/ -type f -name "*.html" -exec rm {} \;
+
+npm run generate-toc
+
+echo 'remove old pdf'
+rm "CHIRIMEN Raspberry Pi Zero W チュートリアル.pdf"
 
 echo 'build start'
 npm run build
