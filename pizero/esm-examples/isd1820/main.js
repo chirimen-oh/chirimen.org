d18f4bd38d256ae7550e41e2d3e4eead41081e48
Satoru Takagi <satakagi@gmail.com>
2023-12-28 15:28:39 +0900

    Add ISD1820

---
+++ pizero/esm-examples/isd1820/main.js
@@ -0,0 +1,18 @@
+import {requestGPIOAccess} from "./node_modules/node-web-gpio/dist/index.js";
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
+
+async function blink() {
+  const gpioAccess = await requestGPIOAccess();
+  const port = gpioAccess.ports.get(26);
+
+  await port.export("out");
+
+  for (;;) {
+    await port.write(1);
+    await sleep(3000); // 3秒間再生
+    await port.write(0);
+    await sleep(1000); // 1秒間停止
+  }
+}
+
+blink();
