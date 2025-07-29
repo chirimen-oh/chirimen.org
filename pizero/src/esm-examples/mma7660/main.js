d9173f2e82b4d92a0ba84d30297c284289e3da41
ms-inoue-dev <m.inoue.0802@gmail.com>
2025-07-29 13:43:54 +0900

    mma7660のExampleを追加

---
+++ pizero/src/esm-examples/mma7660/main.js
@@ -0,0 +1,22 @@
+import {requestI2CAccess} from "chirimen";
+import MMA7660 from "@chirimen/mma7660";
+
+const i2cAccess = await requestI2CAccess();
+
+const i2cPort = i2cAccess.ports.get(1);
+
+const mma7660 = new MMA7660(i2cPort, 0x4c);
+
+await mma7660.init();
+
+setInterval(async() => {
+    let XYZData = await mma7660.getXYZ();
+    let AccelerationData = await mma7660.getAcceleration();
+
+    console.dir("x =" + XYZData.X);
+    console.dir("y =" + XYZData.Y);
+    console.dir("z =" + XYZData.Z);
+
+    console.dir("accleration of X/Y/Z: " + AccelerationData.X + " g/ " + AccelerationData.Y + " g/ " + AccelerationData.Z + " g");
+
+}, 500);
