dd865b9482b1828169a56c281afed1857baf7665
ms-inoue-dev <132976195+ms-inoue-dev@users.noreply.github.com>
2025-07-31 10:05:15 +0900

    Grove mma7660のExampleを追加 (#148)
    * mma7660のExampleを追加
    
    * eslintの指摘に合わせてmma7660のExampleを修正
    
    readmeに参考URL等を追記
    
    * mma7660のreadmeにファイル説明、出力単位の説明を追加
    
    * mma7660のExample内にあった誤字を修正
    
    ---------
    
    Co-authored-by: D.F.Mac. @TripArts Music <tadfmac@users.noreply.github.com>

---
+++ pizero/src/esm-examples/mma7660/main.js
@@ -0,0 +1,28 @@
+import {requestI2CAccess} from "chirimen";
+import MMA7660 from "@chirimen/mma7660";
+
+async function main() {
+
+    const i2cAccess = await requestI2CAccess();
+
+    const i2cPort = i2cAccess.ports.get(1);
+
+    const mma7660 = new MMA7660(i2cPort, 0x4c);
+
+    await mma7660.init();
+
+    setInterval(async() => {
+        const XYZData = await mma7660.getXYZ();
+        const AccelerationData = await mma7660.getAcceleration();
+
+        console.dir(`X =${  XYZData.X}`);
+        console.dir(`y =${  XYZData.Y}`);
+        console.dir(`z =${  XYZData.Z}`);
+
+        console.dir(`acceleration of X/Y/Z: ${  AccelerationData.X  } g/ ${  AccelerationData.Y  } g/ ${  AccelerationData.Z  } g`);
+
+    }, 500);
+
+}
+
+main();
