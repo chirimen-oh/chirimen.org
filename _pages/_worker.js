d7c2ccec697b8b7669cee8331fd5f8ae6f12ee27
Kohei Watanabe <nebel@fogtype.com>
2025-09-22 14:33:50 +0900

    _worker.jsを新規作成し、リポジトリへのリダイレクト機能を追加

---
+++ _pages/_worker.js
@@ -0,0 +1,28 @@
+const repos = [
+  "chirimen",
+  "chirimen-drivers",
+  "chirimen-micro-bit",
+  "chirimen-TY51822r3",
+  "node-web-gpio",
+  "node-web-i2c",
+  "PiZeroWebSerialConsole",
+  "remote-connection",
+  "technical-book8",
+  "tutorials-TY51822r3",
+  "webGPIO-etc-on-microbit-via-webBluetooth",
+];
+
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+
+    for (const repo of repos) {
+      if (url.pathname === `/${repo}` || url.pathname.startsWith(`/${repo}/`)) {
+        url.hostname = "www.chirimen.org";
+        return Response.redirect(url, 301);
+      }
+    }
+
+    return fetch(request);
+  },
+};
