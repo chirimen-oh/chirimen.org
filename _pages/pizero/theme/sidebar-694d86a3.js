9ec1073d35aa29fe55d1523f31d99b722e1bddfc
Kohei Watanabe <nebel@fogtype.com>
2025-12-18 18:16:03 +0900

    fixed broken sidebar logo

--- _pages/pizero/theme/sidebar-694d86a3.js
+++ _pages/pizero/theme/sidebar-694d86a3.js
@@ -2,7 +2,8 @@
 
 // sidebar にロゴ画像とトップページへのリンクを追加
 window.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#sidebar .sidebar-scrollbox")
+  document
+    .querySelector(".sidebar .sidebar-scrollbox")
     ?.insertAdjacentHTML(
       "afterbegin",
       `<a href="/"><img alt="CHIRIMEN" src="logo.png" height="60"></a>`,
