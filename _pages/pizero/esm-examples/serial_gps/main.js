65a944d9e434b0f0b092e28a8bca42c634115b64
bot <>
2025-12-18 08:40:19 +0000

    bot: update pages

--- _pages/pizero/esm-examples/serial_gps/main.js
+++ _pages/pizero/esm-examples/serial_gps/main.js
@@ -1,8 +1,7 @@
 // シリアルからGPSデータ受信
 import { SerialPort } from 'serialport';
 import { ReadlineParser } from '@serialport/parser-readline';
-import GPSpackage from 'gps';
-const GPS = GPSpackage.GPS;
+import { GPS } from 'gps';
 
 const port = new SerialPort({ path: '/dev/ttyS0', baudRate: 9600 })
 const parser = port.pipe(new ReadlineParser())
