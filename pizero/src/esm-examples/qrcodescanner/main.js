0c050a669e48b650f97244367a8f56d69af8cdec
Kohei Watanabe <nebel@fogtype.com>
2026-08-07 22:21:22 +0900

    Update QR scanner package import path

--- pizero/src/esm-examples/qrcodescanner/main.js
+++ pizero/src/esm-examples/qrcodescanner/main.js
@@ -1,8 +1,7 @@
 import { requestI2CAccess } from "node-web-i2c";
-import QRScanner from "@chirimen/qrscanner";
+import QRScanner from "@chirimen/qrcodescanner";
 const i2cAccess = await requestI2CAccess();
-const i2cPort = i2cAccess.ports.get(1);
-const qrscanner = new QRScanner(i2cPort, 0x21);
+const qrscanner = new QRScanner(i2cAccess.ports.get(1), 0x21);
 await qrscanner.init();
 await qrscanner.setTriggerMode(0);
 
