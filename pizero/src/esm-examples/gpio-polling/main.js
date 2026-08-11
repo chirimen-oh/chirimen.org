7f2ac6869cfc903a0f212728c65ed3841db85b8a
Kohei Watanabe <nebel@fogtype.com>
2026-08-11 22:35:07 +0900

    Refactor GPIO polling code for consistency

--- pizero/src/esm-examples/gpio-polling/main.js
+++ pizero/src/esm-examples/gpio-polling/main.js
@@ -1,13 +1,13 @@
 import { requestGPIOAccess } from "node-web-gpio";
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-  const gpioAccess = await requestGPIOAccess();
-  const gpioPort = gpioAccess.ports.get(5);
+const gpioAccess = await requestGPIOAccess();
+const gpioPort = gpioAccess.ports.get(5);
 
-  await gpioPort.export("in");
+await gpioPort.export("in");
 
-  while (true) {
-    const v = await gpioPort.read();
-	console.log(v);
-    await sleep(300);
-  }
+while (true) {
+  const v = await gpioPort.read();
+  console.log(v);
+  await sleep(300);
+}
